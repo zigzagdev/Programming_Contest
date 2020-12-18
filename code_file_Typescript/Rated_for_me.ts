a887c113d8dc4b377299673516b114a0c3952a2c
Masatoshi <msts.oo0131@gmail.com>
2020-12-18 14:59:10 +0900

    Rated_for_me_submitted

---
+++ code_file_Typescript/Rated_for_me.ts
@@ -0,0 +1,12 @@
+function Main(input){
+    var a = parseInt(input,10);
+    if(a<1200){
+        console.log('ABC')
+    }else if(a<2800){
+        console.log("ARC")
+    }else{
+        console.log("AGC")
+    }
+
+}
+Main(require('fs').readFileSync('/dev/stdin','utf8'));
