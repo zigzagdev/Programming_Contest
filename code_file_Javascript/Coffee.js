b7b9572d90ccd3df340e22c25e19c78b8788d968
Masatoshi <msts.oo0131@gmail.com>
2021-01-04 16:01:33 +0900

    file_name_changed

--- code_file_Javascript/Coffee.js
+++ code_file_Javascript/Coffee.js
@@ -1,5 +1,8 @@
 function Main(input){
 
-    input[2]==input[3]&& input[4]==input[5] ? console.log("Yes"): console.log("No") ;}
+    input[2]==input[3]&& input[4]==input[5] ?
+
+
+        console.log("Yes"): console.log("No") ;}
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
