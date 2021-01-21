23d86fc9485dc32ef7d3f1c552c7aedad1359d6f
Masatoshi <msts.oo0131@gmail.com>
2021-01-21 12:35:21 +0900

    119.Still.TBD.rb_submitted

---
+++ Atcorder/code_file_Ruby/119.Still.TBD.rb
@@ -0,0 +1,7 @@
+S=gets.chomp
+
+if S <  "2019/05/01"
+  puts "Heisei"
+else
+  puts "TBD"
+end
--- Atcorder/code_file_Ruby/120.Favorite.Sound.rb
+++ Atcorder/code_file_Ruby/120.Favorite.Sound.rb
@@ -1,1 +1,8 @@
+a,b,c=gets.split.map(&:to_i)
 
+if b-a*c >0
+  puts c
+else
+  b-a*c <0
+  puts b/a
+end
