e0f8290d26d72e29378900045c4d0a0a342a9a75
Masatoshi <msts.oo0131@gmail.com>
2021-01-24 14:41:28 +0900

    115.Christmas.Eve.Eve.Eve.rb_submitted

---
+++ Atcorder/code_file_Ruby/115.ABC/115.A.Christmas.Eve.Eve.Eve.rb
@@ -0,0 +1,12 @@
+D=gets.chomp.to_i
+
+case when  D==25
+       puts "Christmas"
+when D==24
+  puts "Christmas Eve"
+when D==23
+  puts "Christmas Eve Eve"
+when D==22
+  puts "Christmas Eve Eve Eve"
+end
+
