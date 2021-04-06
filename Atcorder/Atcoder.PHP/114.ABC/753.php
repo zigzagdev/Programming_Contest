17ad7f8c99f079addad29194b1927d3ffbcbbe8c
Masatoshi <msts.oo0131@gmail.com>
2021-04-06 16:42:17 +0900

    PHP_A_solved

---
+++ Atcorder/Atcoder.PHP/114.ABC/753.php
@@ -0,0 +1,7 @@
+<?php
+fscanf(STDIN, "%d", $m);
+if($m==7||$m==5||$m==3) {
+    echo "YES" ;} else{
+    echo "NO" ;}
+end
+?>
---
+++ Atcorder/Atcoder.PHP/115.ABC/A.ChristmasEveEveEve.php
@@ -0,0 +1,11 @@
+<?php
+fscanf(STDIN, "%d", $m);
+ if($m==25) {
+    echo 'Christmas' ;} elseif($m==24){
+    echo 'Christmas Eve';} elseif($m==23){
+    echo 'Christmas Eve Eve';} else{
+    echo 'Christmas Eve Eve Eve' ;}
+
+ end
+?>
+
