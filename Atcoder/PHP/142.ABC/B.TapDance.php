a7af6ba7afb4bcab1cb622052587afd42a1e69af
Masatoshi <msts.oo0131@gmail.com>
2021-04-16 13:56:21 +0900

    142.B_Solved

---
+++ Atcoder/PHP/142.ABC/B.TapDance.php
@@ -0,0 +1,12 @@
+<?php
+fscanf(STDIN, "%s", $s);
+$count=good;
+for($i=0; $i<strlen($s); $i++){
+    if($i%2 == 0 && $s[$i] == "L"|| $i%2 == 1 && $s[$i] == "R"){
+        $count =bad; break;}
+}
+if($count==good){
+    echo 'Yes';} else {
+    echo 'No';}
+?>
+
