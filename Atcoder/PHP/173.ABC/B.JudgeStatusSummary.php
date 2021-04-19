24e0542029f03d44d871fc5a82d1e10642d782a3
Masatoshi <msts.oo0131@gmail.com>
2021-04-19 14:32:11 +0900

    PHP_B_173_submitted

---
+++ Atcoder/PHP/173.ABC/B.JudgeStatusSummary.php
@@ -0,0 +1,22 @@
+<?php
+fscanf(STDIN, "%d", $m);
+$AC=0;
+$WA=0;
+$TLE=0;
+$RE=0;
+
+
+for($i =0;$i<$m; $i++){
+    $j = trim(fgets(STDIN));
+    if($j=="AC"){
+        $AC ++;} elseif($j=="WA"){
+        $WA ++;} elseif($j=="TLE"){
+        $TLE ++;} else{
+        $RE ++ ;}
+}
+
+echo "AC x ".$AC."\n";
+echo "WA x ".$WA."\n";
+echo "TLE x ".$TLE."\n";
+echo "RE x ".$RE."\n";
+?>
---
+++ Atcoder/PHP/196.ABC/B.RoundDown.php
@@ -0,0 +1,3 @@
+<?php
+echo explode(".", trim(fgets(STDIN)))[0];
+?>
