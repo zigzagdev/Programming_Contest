c314c4265107bac2258f35642de6bba4e597995a
Masatoshi <msts.oo0131@gmail.com>
2021-05-05 15:04:06 +0900

    Ruby_solved

---
+++ Atcoder/Ruby/80.ABC/B.HarshadNumber.rb
@@ -0,0 +1,17 @@
+n=gets.to_i
+integer=n
+string=n.to_s
+
+strings=string.split(//)
+
+integers = []
+for n in strings
+  integers.push(n.to_i)
+end
+integers=integers.inject(:+)
+
+if(integer%integers==0)
+  puts "Yes"
+else
+  puts "No"
+end
