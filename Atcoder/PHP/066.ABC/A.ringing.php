783ef5aa336896ceb50497dcff986f7522aab19a
Masatoshi <msts.oo0131@gmail.com>
2021-05-08 12:09:13 +0900

    PHP_B_solved

---
+++ Atcoder/PHP/066.ABC/A.ringing.php
@@ -0,0 +1,10 @@
+<?php
+fscanf(STDIN, "%d%d%d", $n,$a,$b);
+
+$first=($n+$a);
+$second=($a+$b);
+$third=($n+$b);
+
+$array=array($first,$second,$third);
+
+echo min($array);
---
+++ Atcoder/Ruby/156.ABC/B.Digits.rb
@@ -0,0 +1,6 @@
+n,k=gets.split(" ").map &:to_i
+
+num=n
+
+digits=num.to_s(k)
+puts digits.to_s.length
