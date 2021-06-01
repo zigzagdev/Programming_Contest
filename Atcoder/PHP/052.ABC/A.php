55eb4980b234130c34eb0f9ed59eb6d546a034f8
Masatoshi <msts.oo0131@gmail.com>
2021-06-01 13:51:33 +0900

    PHP_A_solved

---
+++ Atcoder/PHP/052.ABC/A.php
@@ -0,0 +1,5 @@
+<?php
+fscanf(STDIN, "%d%d%d%d", $s,$t,$u,$v);
+  if($s*$t<$u*$v)
+   {echo $u*$v;} else
+   {echo $s*$t;}
---
+++ Atcoder/PHP/053.ABC/A.php
@@ -0,0 +1,5 @@
+ fscanf(STDIN, "%d", $s);
+
+ if($s<1200)
+ {echo "ABC";} else
+ {echo "ARC";}
