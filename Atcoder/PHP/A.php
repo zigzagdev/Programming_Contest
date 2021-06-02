5db3458d81da72af4241bf19be58224029e2dcb1
Masatoshi <msts.oo0131@gmail.com>
2021-06-02 13:34:22 +0900

    Atcoder_solved

---
+++ Atcoder/PHP/A.php
@@ -0,0 +1,7 @@
+<?php
+fscanf(STDIN, "%d%s%d", $s,$t,$u);
+
+ if($t=='+')
+  {echo $s + $u ;}
+ else
+  {echo $s - $u ;}
---
+++ Atcoder/Ruby/63.ABC/A.rb
@@ -0,0 +1,9 @@
+s=gets.chomp
+
+ss=s.split(//)
+
+if(ss==ss.uniq)
+  puts 'yes'
+else
+  puts 'no'
+end  
