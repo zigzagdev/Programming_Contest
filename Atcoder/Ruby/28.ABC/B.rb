7f4e742e00f36e2e219cb407689ce6c844176ec3
Masatoshi <msts.oo0131@gmail.com>
2021-07-02 20:27:06 +0900

    Ruby_B_solved

---
+++ Atcoder/Ruby/28.ABC/B.rb
@@ -0,0 +1,10 @@
+moriyama = gets.chomp
+
+kes = moriyama.count('A')
+fuji = moriyama.count('B')
+evis =  moriyama.count('C')
+jap =  moriyama.count('D')
+acho = moriyama.count('E')
+suko =  moriyama.count('F')
+
+puts [kes, fuji,evis,jap,acho,suko].join(' ').squeeze(' '
