dd710e74bb2152abe40ed2318958214c84415f43
Masatoshi <msts.oo0131@gmail.com>
2021-07-09 13:15:17 +0900

    PHP_B_solved

---
+++ Atcoder/PHP/034.ABC/B.php
@@ -0,0 +1,6 @@
+<?php
+fscanf(STDIN,"%d",$moriyama);
+
+ if($moriyama%2 != 0) {
+   echo ($moriyama+1)."\n";} else {
+   echo ($moriyama-1)."\n";}
---
+++ Atcoder/Ruby/34.ABC/B.rb
@@ -0,0 +1,7 @@
+moriyama = gets.to_i
+
+if(moriyama%2 != 0)
+  puts moriyama +1
+else
+  puts moriyama-1
+end  
