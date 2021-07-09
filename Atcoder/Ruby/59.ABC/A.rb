11501dfcb3637f200658c62d82439932d3ade3c3
Masatoshi <msts.oo0131@gmail.com>
2021-07-09 12:42:29 +0900

    Ruby_Solved

---
+++ Atcoder/Ruby/59.ABC/A.rb
@@ -0,0 +1,8 @@
+moriyama,kes,fuji = gets.split(" ").map &:to_s
+
+a = moriyama[0]
+b = kes[0]
+c = fuji[0]
+
+puts a.upcase+b.upcase+c.upcase
+
---
+++ Atcoder/Ruby/59.ABC/B.rb
@@ -0,0 +1,11 @@
+moriyama = gets.to_i
+kes = gets.to_i
+
+if(moriyama>kes)
+  puts 'GREATER'
+elsif(moriyama == kes)
+  puts 'EQUAL'
+else
+  puts 'LESS'
+end
+
