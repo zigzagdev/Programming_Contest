baead6ae14f11ff756f21a972545c17d3be6f1e9
Masatoshi <msts.oo0131@gmail.com>
2021-07-14 11:08:34 +0900

    Ruby_B_solved

---
+++ Atcoder/Ruby/68.ABC/B.rb
@@ -0,0 +1,18 @@
+tk = gets.to_i
+
+if (64<=tk)
+  puts '64'
+elsif (32<=tk)
+  puts '32'
+elsif (16<=tk)
+  puts '16'
+elsif (8<=tk)
+  puts '8'
+elsif (4<=tk)
+  puts '4'
+elsif (2<=tk)
+  puts '2'
+else
+  puts '1'
+end  
+
