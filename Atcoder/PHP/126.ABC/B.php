e05f0e4843b23e0d9f501dd76c53665df516dadb
Masatoshi <msts.oo0131@gmail.com>
2021-07-19 13:12:12 +0900

    PHP_B_solved

---
+++ Atcoder/PHP/126.ABC/B.php
@@ -0,0 +1,13 @@
+<?php
+fscanf(STDIN,"%s",$kes);
+
+$evis = substr($kes,0,2);
+$jap = substr($kes,-2,2);
+
+$a = '03';
+
+if($evis>'00'&& $evis <'13' && $jap>'00'&& $jap <'13'){
+  echo "AMBIGUOUS";} elseif ($evis>'00'&& $evis <'13') {
+  echo "MMYY";} elseif ($jap>'00'&& $jap <'13') {
+  echo "YYMM";} else {
+  echo "NA";}
