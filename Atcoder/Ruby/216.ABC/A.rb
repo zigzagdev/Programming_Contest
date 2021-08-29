bc40a0bf6576296a48e3dac3b0a7ba894c60e15c
Masatoshi <msts.oo0131@gmail.com>
2021-08-29 23:07:04 +0900

    Ruby_solved

---
+++ Atcoder/Ruby/216.ABC/A.rb
@@ -0,0 +1,9 @@
+mori,yama = gets.split(".").map(&:to_i)
+
+if (yama < 3)
+  puts "#{mori}-"
+elsif (yama < 7)
+  puts "#{mori}"
+else
+  puts "#{mori}+"
+end  
---
+++ Atcoder/Ruby/216.ABC/B.rb
@@ -0,0 +1,8 @@
+kes = gets.to_i
+
+dimaria = kes.times.map{gets.chomp}.sort.uniq.size
+if dimaria == kes
+  puts "No"
+else
+  puts "Yes"
+end  
