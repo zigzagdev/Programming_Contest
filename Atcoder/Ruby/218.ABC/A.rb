226b3e0556d018e4cda97d9b3affb7bc6c54cdef
Masatoshi <msts.oo0131@gmail.com>
2021-09-12 22:18:29 +0900

    218_Ruby_solved

---
+++ Atcoder/Ruby/218.ABC/A.rb
@@ -0,0 +1,8 @@
+kes = gets.to_i
+mori = gets.chomp
+
+if mori[kes-1] == 'o'
+  puts "Yes"
+else
+  puts "No"
+end  
---
+++ Atcoder/Ruby/218.ABC/B.rb
@@ -0,0 +1,9 @@
+kes = gets.chomp.split(' ').map(&:to_i)
+
+key = ("a".."z").to_a
+
+mori = []
+kes.each do |i|
+  mori.push(key[i-1])
+end
+puts mori.join
