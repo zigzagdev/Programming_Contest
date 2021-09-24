c3aabc3b70f19bc6f6e0c980803353d18bf2d472
Masatoshi <msts.oo0131@gmail.com>
2021-09-25 00:18:07 +0900

    PHP_180_B_solved

---
+++ Atcoder/PHP/180.ABC/B.php
@@ -0,0 +1,21 @@
+<?php
+fscanf(STDIN, "%d",$kes);
+$map = explode(" ",trim(fgets(STDIN)));
+
+$moriyama = 0;
+ for($i=0;$i<=count($map);$i++){
+   $moriyama += abs($map[$i]);
+ }
+echo $moriyama.PHP_EOL;
+
+$fuji = 0;
+ for($i=0;$i<=count($map);$i++){
+   $fuji += abs($map[$i]**2);
+ }
+echo sqrt($fuji).PHP_EOL;
+
+$ryo = [];
+ for($i=0;$i<=count($map);$i++){
+   $ryo[] = abs($map[$i]);
+ }
+echo max($ryo);
