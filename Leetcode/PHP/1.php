36edefae552573869e6e41cc003498389f7abc8c
zigzagdev <msts.oo0131@gmail.com>
2022-05-16 22:54:21 +0900

    Add_LeetCode

---
+++ Leetcode/PHP/1.php
@@ -0,0 +1,11 @@
+
+class Solution {
+    function twoSum($nums, $target) {
+        for($i=0;$i<count($nums);$i++){
+            for($k=$i+1;$k<count($nums);$k++){
+                if($nums[$i]+$nums[$k] == $target){
+                    return [$i, $k];
+                }
+            }
+        }
+    }
