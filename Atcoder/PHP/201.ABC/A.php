22eb65fd2d857abc22c4165273b0eec7a3afbe0a
zigzagdev <msts.oo0131@gmail.com>
2022-07-23 13:37:28 +0900

    solve_ABC201

---
+++ Atcoder/PHP/201.ABC/A.php
@@ -0,0 +1,12 @@
+<?php
+fscanf(STDIN, "%d%d%d", $kes, $mori, $yama);
+$evis = [$kes, $mori, $yama];
+asort($evis);
+$evisjap = [];
+foreach($evis as $evija => $taku){
+  array_push($evisjap, $taku);
+}
+if($evisjap[2]-$evisjap[1] != $evisjap[1]-$evisjap[0]){
+  echo 'No';} else {
+  echo 'Yes';
+}
---
+++ Atcoder/PHP/201.ABC/A.tpl
@@ -0,0 +1,13 @@
+<?php
+fscanf(STDIN, "%d", $kes);
+$evis = [];
+for($i=0;$i<$kes;$i++){
+    fscanf(STDIN, "%s %d", $mori, $yama);
+    $evis[$mori] = $yama;
+}
+arsort($evis);
+$evisjap = [];
+foreach($evis as $taku => $evija){
+  array_push($evisjap, $taku);
+}
+echo $evisjap[1];
