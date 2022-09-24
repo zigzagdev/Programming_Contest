86d75dbb42c6a6256b0678261d6adb872106c7c7
zigzagdev <msts.oo0131@gmail.com>
2022-09-24 14:19:57 +0900

    soluve_leetcode_217

---
+++ Leetcode/PHP/217.php
@@ -0,0 +1,23 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        sort($nums);
+        $counts = array_count_values($nums);
+        print_r($counts);
+        foreach($counts as $count){
+            if($count != 1){
+                return 'true';
+                break;
+            }
+            echo 'false';
+        }
+    }
+}
+
+?>
